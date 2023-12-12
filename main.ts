7580fd32aa2130fa9235d98c0db08a9034df825d
Peap <123750838+PeapGit@users.noreply.github.com>
2023-12-12 14:24:19 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 namespace SpriteKind {
     export const lighting = SpriteKind.create()
     export const lighting2 = SpriteKind.create()
+    export const lighting3 = SpriteKind.create()
+    export const lighting4 = SpriteKind.create()
+    export const lighting5 = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     info.changeLifeBy(-1)
@@ -25,6 +28,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, l
     info.setLife(9)
     sprites.destroy(lighting)
     sprites.destroy(lighting2)
+    sprites.destroy(lighting_3)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level7`)
@@ -44,6 +48,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
     tiles.placeOnRandomTile(mySprite, assets.tile`myTile0`)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile11`)
     lighting = sprites.create(img`
         d 
         `, SpriteKind.lighting)
@@ -52,9 +57,27 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, l
         c 
         `, SpriteKind.lighting2)
     tiles.placeOnRandomTile(lighting2, assets.tile`myTile6`)
+    lighting_3 = sprites.create(img`
+        2 
+        `, SpriteKind.lighting3)
+    tiles.placeOnRandomTile(lighting_3, assets.tile`myTile7`)
+    lighting_4 = sprites.create(img`
+        5 
+        `, SpriteKind.lighting4)
+    tiles.placeOnRandomTile(lighting_4, assets.tile`myTile10`)
+    lighting_5 = sprites.create(img`
+        8 
+        `, SpriteKind.lighting5)
+    tiles.placeOnRandomTile(lighting_5, assets.tile`myTile12`)
 })
+let projectile5: Sprite = null
+let projectile4: Sprite = null
+let projectile3: Sprite = null
 let projectile: Sprite = null
 let projectile2: Sprite = null
+let lighting_5: Sprite = null
+let lighting_4: Sprite = null
+let lighting_3: Sprite = null
 let lighting2: Sprite = null
 let lighting: Sprite = null
 let mySprite: Sprite = null
@@ -182,5 +205,86 @@ forever(function () {
         `, lighting, 0, 0)
     pause(1000)
     sprites.destroy(projectile)
+    pause(3000)
+})
+forever(function () {
+    projectile3 = sprites.createProjectileFromSprite(img`
+        ....9.....9....9....
+        ....9.....99...9....
+        ....99.....9..99....
+        .....99....99.9.....
+        999...9.....999.....
+        ..99999......9.....9
+        ......99....99....99
+        99.....9....9....99.
+        .999...999.9999..9..
+        ...99....999..999999
+        ....99999999.......9
+        99999....999........
+        .........9.9........
+        ........99.99.......
+        .......99...99.....9
+        ......99.....9999999
+        ....999.......9.....
+        ..999.9.......99....
+        999...9........9....
+        9.....9......9999...
+        `, lighting_3, 0, 0)
+    pause(1000)
+    sprites.destroy(projectile3)
+    pause(3000)
+})
+forever(function () {
+    projectile4 = sprites.createProjectileFromSprite(img`
+        ....9.....9....9....
+        ....9.....99...9....
+        ....99.....9..99....
+        .....99....99.9.....
+        999...9.....999.....
+        ..99999......9.....9
+        ......99....99....99
+        99.....9....9....99.
+        .999...999.9999..9..
+        ...99....999..999999
+        ....99999999.......9
+        99999....999........
+        .........9.9........
+        ........99.99.......
+        .......99...99.....9
+        ......99.....9999999
+        ....999.......9.....
+        ..999.9.......99....
+        999...9........9....
+        9.....9......9999...
+        `, lighting_4, 0, 0)
+    pause(1000)
+    sprites.destroy(projectile4)
+    pause(3000)
+})
+forever(function () {
+    projectile5 = sprites.createProjectileFromSprite(img`
+        ....9.....9....9....
+        ....9.....99...9....
+        ....99.....9..99....
+        .....99....99.9.....
+        999...9.....999.....
+        ..99999......9.....9
+        ......99....99....99
+        99.....9....9....99.
+        .999...999.9999..9..
+        ...99....999..999999
+        ....99999999.......9
+        99999....999........
+        .........9.9........
+        ........99.99.......
+        .......99...99.....9
+        ......99.....9999999
+        ....999.......9.....
+        ..999.9.......99....
+        999...9........9....
+        9.....9......9999...
+        `, lighting_5, 0, 0)
+    pause(1000)
+    sprites.destroy(projectile5)
     pause(3000)
 })
